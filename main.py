614672238fb4b7b89df739fc3c11569f09566c61
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2020-06-10 01:37:12 +1000

    Добавил функцию "get_popword"
    Она выводит топ-5 самых часто используемых слов на этом канале

--- main.py
+++ main.py
@@ -59,6 +59,46 @@ async def get_info(ctx):
     await ctx.send('Держи, хорошего дня:3', file=discord.File('info.jpg'))
     # Отправляем сообщение
 
+    
+@client.command()
+async def get_popword(ctx):
+    messages_data = []
+    # Список русских слов
+    async for message in discord.TextChannel.history(ctx, limit=None):
+        if message.author != client.user and not message.content.startswith('!'):
+            messages_data.append(message.content)
+    # Если сообщение не от бота и не является командой, то добавляем
+    messages_data = list(filter(lambda x: x != '',
+                                re.sub(r"[^А-Яа-яЁё ]*", '',
+                                       re.sub(r"[.,/+\"\'!:;]", ' ', ' '.join(messages_data))).split(' ')))
+    # Убираем знаки препинания, оставляем только слова из русских букв)
+    for i in range(len(messages_data)):
+        messages_data[i] = (morph.parse(messages_data[i])[0]).normal_form
+    # Приводим слова в исходную форму
+    data = {}
+    for i in Counter(messages_data):
+        data[i] = messages_data.count(i)
+    # Формируем словарь, ключами которого являются слова, а значениями - количество их повторений
+    lst = list(data.items())
+    lst.sort(key=lambda i: i[1], reverse=True)
+    # Создаём из словаря список кортежей вида "(слово, кол-во повторений)" для того, чтобы его можно было отсортировать.
+    labels = [lst[0][0].capitalize(), lst[1][0].capitalize(),
+              lst[2][0].capitalize(), lst[3][0].capitalize(), lst[4][0].capitalize()]
+    # Названия столбцов (сами слова)
+    means = [lst[0][1], lst[1][1], lst[2][1], lst[3][1], lst[4][1], ]
+    # Значение каждого столбца
+    width = 0.35
+    # Ширина каждого столбца
+    fig, ax = plt.subplots()
+    ax.bar(labels, means, width)
+    # Создаём график
+    ax.set_ylabel('Кол-во повторений')
+    # Название отметок по оси y
+    ax.set_title('Топ-5 самых часто используемых слов')
+    # Название графика
+    plt.savefig('popword.png')
+    await ctx.send(f'Похоже, что слово "{lst[0][0]}" лидирует!', file=discord.File('popword.png'))
+    # Сохраняем + отправляем
 
 client.run('INSERT_TOKEN')
 # Запуск бота)
