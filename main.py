bd52c259b9c0b603b3962a101e82544d0c8ca2d2
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2020-06-10 03:04:08 +1000

    Переработал функцию "get_popword"
    Переработал отбор слов для функции, а также присоединил её к функции "get_info". График для функции "get_info" обзавёлся названием.

--- main.py
+++ main.py
@@ -54,51 +54,28 @@ async def get_info(ctx):
     # Создаем график типа bar
     plt.xticks(x, ('Сущ', 'Мест', 'Глаг', 'Прил', 'Нареч', 'Пред', 'Союз'))
     # Подписываем столбцы
-    plt.savefig('info.jpg')
+    ax.set_title('Количество слов по частям речи:')
+    plt.savefig('info.png')
     # Сохраняем график
-    await ctx.send('Держи, хорошего дня:3', file=discord.File('info.jpg'))
-    # Отправляем сообщение
 
-    
-@client.command()
-async def get_popword(ctx):
-    messages_data = []
-    # Список русских слов
-    async for message in discord.TextChannel.history(ctx, limit=None):
-        if message.author != client.user and not message.content.startswith('!'):
-            messages_data.append(message.content)
-    # Если сообщение не от бота и не является командой, то добавляем
-    messages_data = list(filter(lambda x: x != '',
-                                re.sub(r"[^А-Яа-яЁё ]*", '',
-                                       re.sub(r"[.,/+\"\'!:;]", ' ', ' '.join(messages_data))).split(' ')))
-    # Убираем знаки препинания, оставляем только слова из русских букв)
     for i in range(len(messages_data)):
         messages_data[i] = (morph.parse(messages_data[i])[0]).normal_form
     # Приводим слова в исходную форму
     data = {}
     for i in Counter(messages_data):
-        data[i] = messages_data.count(i)
+        if "NPRO" not in morph.parse(i)[0].tag and "PREP" not in morph.parse(i)[0].tag \
+                and "CONJ" not in morph.parse(i)[0].tag:
+            data[i] = messages_data.count(i)
     # Формируем словарь, ключами которого являются слова, а значениями - количество их повторений
     lst = list(data.items())
     lst.sort(key=lambda i: i[1], reverse=True)
     # Создаём из словаря список кортежей вида "(слово, кол-во повторений)" для того, чтобы его можно было отсортировать.
-    labels = [lst[0][0].capitalize(), lst[1][0].capitalize(),
-              lst[2][0].capitalize(), lst[3][0].capitalize(), lst[4][0].capitalize()]
-    # Названия столбцов (сами слова)
-    means = [lst[0][1], lst[1][1], lst[2][1], lst[3][1], lst[4][1], ]
-    # Значение каждого столбца
-    width = 0.35
-    # Ширина каждого столбца
-    fig, ax = plt.subplots()
-    ax.bar(labels, means, width)
-    # Создаём график
-    ax.set_ylabel('Кол-во повторений')
-    # Название отметок по оси y
-    ax.set_title('Топ-5 самых часто используемых слов')
-    # Название графика
-    plt.savefig('popword.png')
-    await ctx.send(f'Похоже, что слово "{lst[0][0]}" лидирует!', file=discord.File('popword.png'))
-    # Сохраняем + отправляем
+    comment = morph.parse('повторение')[0]
+    await ctx.send(f'Приуэт! Топ-5 слов на этом канале: \n 1.) "{lst[0][0]}", {lst[0][1]} {comment.make_agree_with_number(lst[0][1]).word} \n'
+                   f'2.) "{lst[1][0]}", {lst[1][1]} {comment.make_agree_with_number(lst[1][1]).word} \n 3.) "{lst[2][0]}", {lst[2][1]} {comment.make_agree_with_number(lst[2][1]).word} \n'
+                   f'4.) "{lst[3][0]}", {lst[3][1]} {comment.make_agree_with_number(lst[3][1]).word} \n 5.) "{lst[4][0]}", {lst[4][1]} {comment.make_agree_with_number(lst[4][1]).word} \n',
+                   file=discord.File('info.png'))
+    # Приводим слова в соответствие с чилительными и отправляем
 
 client.run('INSERT_TOKEN')
 # Запуск бота)
